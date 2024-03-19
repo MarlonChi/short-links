7e2ab77157f90e40a6a284d8ab27af016e32eba4
MarlonChi <marlonchiodelli@hotmail.com>
2024-03-19 17:04:00 -0300

    feat: setup

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
-console.log('Hello world')
+import fastify from "fastify";
+
+const app = fastify();
+
+app.get('/teste', () => {
+    return 'Hello world!'
+})
+
+app.listen({
+    port: 3333,
+}).then(() => {console.log('HTTP serrver running!')})
